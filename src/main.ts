3ee89f8d0cd00dda6cb4e0b850e9858d93a3b40a
apomalyn <chretienxavier42@gmail.com>
2022-04-06 00:01:08 -0400

    Add TODO

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,51 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as helper from './helper';
+import {SemanticVersion} from './models/SemanticVersion';
+import {wait} from './wait';
 
 async function run(): Promise<void> {
+  if (github.context.eventName !== 'pull_request') {
+    core.setFailed(
+      "Event isn't a pull request. This action can only work on pull request."
+    );
+  }
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const patch_label: string = core.getInput('patch_label');
+    const minor_label: string = core.getInput('minor_label');
+    const major_label: string = core.getInput('major_label');
+    const look_for: string = core.getInput('look_for_tag');
+    const file_path: string = core.getInput('file_path');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (file_path === '') {
+      core.setFailed(`file_path need to be specified.`);
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const content = helper.loadFile(file_path);
+    const version_raw = content[look_for as keyof typeof content].toString();
+
+    if (version_raw === undefined) {
+      core.setFailed(`Tag ${look_for} not found in ${file_path}`);
+    }
+    core.debug(`File loaded and raw version found: ${version_raw}`);
+
+    const version = SemanticVersion.fromString(version_raw);
+    core.debug(`Raw version parsed to ${version.raw}`);
+
+    // TODO check which label is present on the PR.
+
+    // TODO increment version
+
+    // TODO Write to file
+
+    // TODO check if need comment ==> comment
+
+    // TODO check if need commit => commit
+
+    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
