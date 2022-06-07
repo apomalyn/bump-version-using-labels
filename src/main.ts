49b6d95b31d792c669a2860a6672f0e2c04d6a19
Xavier Chrétien <chretienxavier42@gmail.com>
2022-06-06 22:21:38 -0400

    Ready for the first test run

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,186 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as helper from './helper';
-import {SemanticVersion} from './models/SemanticVersion';
-import {wait} from './wait';
+import GithubService from './service/github-service';
+import { SemanticVersion } from './models/semantic-version';
+import { VersionType } from './models/version-type';
+import { writeFile } from './helper';
+
+const github_service = new GithubService();
+
+const OLD_TAG = '{old}';
+const NEW_TAG = '{new}';
 
 async function run(): Promise<void> {
   if (github.context.eventName !== 'pull_request') {
     core.setFailed(
       "Event isn't a pull request. This action can only work on pull request."
     );
+    return;
   }
   try {
-    const patch_label: string = core.getInput('patch_label');
-    const minor_label: string = core.getInput('minor_label');
-    const major_label: string = core.getInput('major_label');
+    const comment_pr = core.getBooleanInput('comment');
+    const labels = [
+      core.getInput('patch_label'),
+      core.getInput('minor_label'),
+      core.getInput('major_label')
+    ];
     const look_for: string = core.getInput('look_for_tag');
     const file_path: string = core.getInput('file_path');
 
     if (file_path === '') {
       core.setFailed(`file_path need to be specified.`);
+      return;
     }
 
     const content = helper.loadFile(file_path);
-    const version_raw = content[look_for as keyof typeof content].toString();
+    const version = getVersionFromFile(content, look_for);
 
-    if (version_raw === undefined) {
+    if (version === undefined) {
       core.setFailed(`Tag ${look_for} not found in ${file_path}`);
+      return;
     }
-    core.debug(`File loaded and raw version found: ${version_raw}`);
-
-    const version = SemanticVersion.fromString(version_raw);
     core.debug(`Raw version parsed to ${version.raw}`);
 
-    // TODO check which label is present on the PR.
+    const reference_version = await getReferenceVersion(file_path, look_for);
 
-    // TODO increment version
+    // Retrieving the reference version failed
+    if (reference_version === undefined) {
+      return;
+    }
 
-    // TODO Write to file
+    let increased = false;
+    let message = `There is no version labels on the PR. 
+    Please use one of the following: ${labels}`;
 
-    // TODO check if need comment ==> comment
+    core.debug('Start label search');
+    for (const label of github_service.labels) {
+      const index = labels.indexOf(label.name);
+
+      if (index !== -1) {
+        if (increased) {
+          message = `There are multiple version labels on the PR. Please use only one.`;
+          core.setFailed(message);
+          if (comment_pr) {
+            await github_service.createComment(message);
+          }
+          return;
+        }
+        core.debug(`Version label found, increasing ${VersionType[index]}`);
+        reference_version.increase(
+          VersionType[VersionType[index] as keyof typeof VersionType]
+        );
+        increased = true;
+      }
+    }
+
+    if (!increased) {
+      core.setFailed(message);
+      if (comment_pr) {
+        await github_service.createComment(message);
+      }
+      return;
+    }
+
+    Object.defineProperty(content, look_for, {
+      value: reference_version.raw
+    });
+
+    writeFile(file_path, content);
+
+    if (comment_pr) {
+      await github_service.createComment(
+        core
+          .getInput('comment_message')
+          .replace(OLD_TAG, version.raw)
+          .replace(NEW_TAG, reference_version.raw)
+      );
+    }
 
     // TODO check if need commit => commit
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
+  }
+}
+
+function getVersionFromFile(
+  content: Object,
+  key: string
+): SemanticVersion | undefined {
+  const version_raw = content[key as keyof typeof content].toString();
+
+  if (version_raw === undefined) {
+    return;
+  }
+  core.debug(`File loaded and raw version found: ${version_raw}`);
+
+  return SemanticVersion.fromString(version_raw);
+}
+
+async function getReferenceVersion(
+  file_path: string,
+  look_for_key: string
+): Promise<SemanticVersion | undefined> {
+  const use_tag_as_ref: boolean = core.getBooleanInput('use_tag_as_ref');
+
+  if (!use_tag_as_ref) {
+    const branch_name: string = core.getInput('reference_branch');
+    return getRefVersionFromBranch(branch_name, file_path, look_for_key);
+  } else {
+    core.debug(`Retrieving reference version from tags`);
+    return getRefVersionFromTag();
+  }
+}
+
+/**
+ * Retrieve the version associated with [look_for_key] in [file_path] on the
+ * specified branch.
+ */
+async function getRefVersionFromBranch(
+  branch_name: string,
+  file_path: string,
+  look_for_key: string
+): Promise<SemanticVersion | undefined> {
+  try {
+    const reference_file = await github_service.getFileContentForBranch(
+      file_path,
+      branch_name
+    );
+
+    const content: Object = helper.parseFile(
+      reference_file.name,
+      Buffer.from(reference_file.content, 'base64').toString('binary')
+    );
+
+    return getVersionFromFile(content, look_for_key);
+  } catch (e) {
+    if (e instanceof ReferenceError) {
+      core.setFailed(e.message);
+    }
+    return;
+  }
+}
+
+/**
+ * Retrieve the version from the latest tag published
+ */
+async function getRefVersionFromTag(): Promise<SemanticVersion | undefined> {
+  try {
+    const tags_list = await github_service.getRepoTags();
+
+    if (tags_list.length === 0) {
+      core.setFailed(`No tags found on the repository`);
+      return;
+    }
+
+    return SemanticVersion.fromString(tags_list[0].name);
+  } catch (e) {
+    if (e instanceof Error) {
+      core.setFailed(`No tags found on the repository`);
+      return;
+    }
   }
 }
 
---
+++ src/service/github-service.ts
@@ -0,0 +1,79 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as helper from '../helper';
+import { Label, PullRequestEvent } from '@octokit/webhooks-definitions/schema';
+import IGithubGetContentPayload from '../models/igithub-get-content-payload';
+import ITagPayload from '../models/igithub-tag-payload';
+
+export default class GithubService {
+  public readonly eventPayload: PullRequestEvent;
+  private _octokit = helper.getOctokitAuth();
+
+  constructor() {
+    this.eventPayload = github.context.payload as PullRequestEvent;
+  }
+
+  get labels(): Label[] {
+    return this.eventPayload.pull_request.labels;
+  }
+
+  /**
+   * Create a comment on the pull request.
+   */
+  async createComment(message: string): Promise<void> {
+    await this._octokit.rest.issues.createComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: this.eventPayload.pull_request.number,
+      body: message
+    });
+  }
+
+  /**
+   * Retrieve content from a repository branch.
+   * Can throw an ReferenceError if the branch or the file in the branch doesn't
+   * exist.
+   */
+  async getFileContentForBranch(
+    file_path: string,
+    branch_name: string
+  ): Promise<IGithubGetContentPayload> {
+    const branch_exist = await this._octokit.rest.repos.getBranch({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      branch: branch_name
+    });
+    if (branch_exist.status !== 200) {
+      throw new ReferenceError(`Reference branch ${branch_name} not found`);
+    }
+    core.debug(`Reference version will be searched in ${branch_name} branch`);
+
+    const reference_file_response = await this._octokit.rest.repos.getContent({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      path: file_path,
+      ref: branch_name
+    });
+
+    if (reference_file_response.status !== 200) {
+      throw new ReferenceError(
+        `File ${file_path} in branch ${branch_name} not found`
+      );
+    }
+
+    return reference_file_response.data as IGithubGetContentPayload;
+  }
+
+  async getRepoTags(): Promise<ITagPayload[]> {
+    const tags_list_response = await this._octokit.rest.repos.listTags({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    });
+
+    if (tags_list_response.status !== 200) {
+      throw new Error(`No tags found on the repository`);
+    }
+
+    return tags_list_response.data as ITagPayload[];
+  }
+}
