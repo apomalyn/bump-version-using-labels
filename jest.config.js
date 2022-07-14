de4a128f90093434375f1e38065c66c9653a712b
Xavier Chrétien <chretienxavier42@gmail.com>
2022-07-14 15:41:56 -0400

    Change jest to support .spec.ts files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.spec.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  verbose: true
+  verbose: true,
+  "moduleNameMapper": {
+    "^@services/(.*)$": "<rootDir>/src/services/$1",
+    "^@fileHandlers/(.*)$": "<rootDir>/src/file-handlers/$1",
+    "^@models/(.*)$": "<rootDir>/src/models/$1",
+  }
 }
