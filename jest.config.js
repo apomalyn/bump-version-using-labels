8ee6faed5142c91e0deda75508b7addd880ea6f6
Xavier Chretien <xavier.chretien@zendesk.com>
2023-03-02 14:58:32 -0500

    #154: Adding file with all the message on it.

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,9 @@ module.exports = {
   "moduleNameMapper": {
     "^@services/(.*)$": "<rootDir>/src/services/$1",
     "^@fileHandlers/(.*)$": "<rootDir>/src/file-handlers/$1",
+    "^@eventHandlers/(.*)$": "<rootDir>/src/event-handlers/$1",
     "^@models/(.*)$": "<rootDir>/src/models/$1",
     "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@constants/(.*)$": "<rootDir>/src/constants/$1",
   }
 }
---
+++ src/constants/messages.ts
@@ -0,0 +1,12 @@
+export const Messages = {
+  noLabelsFound:
+    'There is no version labels on the PR.\nPlease use one of the following: ',
+  startLabelSearch: 'Searching for a version increase label',
+  multipleLabelsFound:
+    'There are multiple version labels on the PR. Please use only one.',
+  labelFound: 'Version label found, increasing ',
+  versionAlreadyUpdated: 'Version already updated. Skipping.'
+};
+
+export const OLD_TAG = '{old}';
+export const NEW_TAG = '{new}';
